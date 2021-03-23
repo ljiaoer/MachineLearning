a7a9d67b902764843ba9a473785c53efaf844d09
武 <18435186613@163.com>
2021-03-23 16:54:55 +0800

    deeplearning

---
+++ Deeplearning/ANN .py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 19 22:03:02 2021
+
+@author: lijiaojiao
+"""
+#Artificial Neural Network
+"""
+#Keras框架会更便捷，相比tensorflow
+#conda install tensorflow
+#conda install -c conda-forge keras
+#conda update --all
+
+
+
+"""
+
+
+
+
+
+
+
+
